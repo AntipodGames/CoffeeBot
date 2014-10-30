adcafc7e0ca3e3aad21c4d1703252731bb9cdec1
LLeGoff <lenilegoff@gmail.com>
2014-10-30 16:48:27 +0100

    mise a jour des graphisme

--- environment.cpp
+++ environment.cpp
@@ -27,8 +27,8 @@ void Environment::init(){
     entityMap.insert(lightS->getID(),lightS);
     entityTypeMap.insert(lightS->get_nom(),lightS->getID());
 
-//    delete robot;
-//    delete binSwitch;
+    //    delete robot;
+    //    delete binSwitch;
 
     emit sendEM(entityMap);
 
@@ -45,6 +45,12 @@ void Environment::reloadLevel(){
     getHero()->setSpeedX(0);
     getHero()->setSpeedY(0);
     emit initSecondPlan();
+//TODO reinitialisation des trigger
+//    for(QMultiMap<std::string,int>::iterator it = entityTypeMap.begin()
+//        ; it != entityTypeMap.end(); it++){
+//        if(it.key() == "triggerOn")
+//            entityMap.value(it.value())->
+//    }
 }
 
 void Environment::setHitbox(std::string str){
@@ -84,39 +90,15 @@ void Environment::run(){
     }
 
 
-<<<<<<< HEAD
-        for(QMap<int,Entite*>::iterator it = entityMap.begin(); it != entityMap.end(); it++){
-
-            //TMP TRIGGER <- TRIGGER ACTUEL
-            TzEllipse tmpTrigger = it.value()->get_trigger();
-
-            //CHECK NUMERO 1 : LE CHECK POST DEPLACEMENT
-            if(tmpTrigger.intersection(IM.GetImage(hitbox),sf::Color::Black)){
-                double thetaMin = tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).first()*PI;
-                double thetaMax = tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).last()*PI;
-                double tmpTheta = (thetaMax - thetaMin) / 2.;
-                if(tmpTheta >= 0.2){
-                    double finalTheta = thetaMax - tmpTheta - PI;
-                    if(finalTheta < -PI)
-                        finalTheta += 2.* PI;
-
-                    double dX = 4. * TAILLE * (1. - cos(tmpTheta)) * cos(finalTheta);
-                    double dY = 4. * TAILLE * (1. - cos(tmpTheta)) * sin(finalTheta);
-                    //                std::cout << dX << "; " << dY << std::endl;
-                    it.value()->get_trigger().move(dX, dY);
-                    it.value()->set_x(it.value()->get_x() + dX);
-                    it.value()->set_y(it.value()->get_y() + dY);
-                }
-=======
     for(QMap<int,Entite*>::iterator it = entityMap.begin(); it != entityMap.end(); it++){
 
         //TMP TRIGGER <- TRIGGER ACTUEL
         TzEllipse tmpTrigger = it.value()->get_trigger();
 
         //CHECK NUMERO 1 : LE CHECK POST DEPLACEMENT
-        if(tmpTrigger.intersection(IM.GetImage("graphics/hitmap2.png"),sf::Color::Black)){
-            double thetaMin = tmpTrigger.intersectTabAngle(IM.GetImage("graphics/hitmap2.png"), sf::Color::Black).first()*PI;
-            double thetaMax = tmpTrigger.intersectTabAngle(IM.GetImage("graphics/hitmap2.png"), sf::Color::Black).last()*PI;
+        if(tmpTrigger.intersection(IM.GetImage(hitbox),sf::Color::Black)){
+            double thetaMin = tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).first()*PI;
+            double thetaMax = tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).last()*PI;
             double tmpTheta = (thetaMax - thetaMin) / 2.;
             if(tmpTheta >= 0.2){
                 double finalTheta = thetaMax - tmpTheta - PI;
@@ -126,113 +108,67 @@ void Environment::run(){
                 double dX = 4. * TAILLE * (1. - cos(tmpTheta)) * cos(finalTheta);
                 double dY = 4. * TAILLE * (1. - cos(tmpTheta)) * sin(finalTheta);
 
-                if(dY < 1. && dY > -1.){
-                    std::cout << "mur ";
-                    if(dX < 0.)
-                        std::cout << "à droite" << std::endl;
-                    else
-                        std::cout << "à gauche" << std::endl;
+//                if(dY < 1. && dY > -1.){
+//                    std::cout << "mur ";
+//                    if(dX < 0.)
+//                        std::cout << "à droite" << std::endl;
+//                    else
+//                        std::cout << "à gauche" << std::endl;
 
-                }
+//                }
 
                 it.value()->get_trigger().move(dX, dY);
                 it.value()->set_x(it.value()->get_x() + dX);
                 it.value()->set_y(it.value()->get_y() + dY);
->>>>>>> bc9fb193c8626b409d8f4d86207eaa18c26efb2f
             }
 
-<<<<<<< HEAD
-            //TMP TRIGGER <- TRIGGER PREVISIONNEL
-            tmpTrigger = it.value()->get_trigger();
-            tmpTrigger.set_centre(it.value()->get_trigger().get_centreX() + it.value()->getSpeedVector().first
-                                  , it.value()->get_trigger().get_centreY() + it.value()->getSpeedVector().second );
-
-            //CHECK NUMERO 2 : LE CHECK PRE DEPLACEMENT
-            if(tmpTrigger.intersection(IM.GetImage(hitbox),sf::Color::Black)){
-                //collision avec un mur a gauche
-                /* if(tmpTrigger.intersectAngle(IM.GetImage(hitbox), sf::Color::Black) <= PI + PI / 4.
-                    && tmpTrigger.intersectAngle(IM.GetImage(hitbox), sf::Color::Black) >= PI - PI / 4.)*/
-                if(PI >= tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).first()*PI
-                        && PI < tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).last()*PI){
-                    it.value()->setSpeedX(0);
-                    //                it.value()->moveRight();
-                }
-
-
-                //collision avec un mur a droite
-                /*if(tmpTrigger.intersectAngle(IM.GetImage(hitbox), sf::Color::Black) <=  PI / 4.
-                    && tmpTrigger.intersectAngle(IM.GetImage(hitbox), sf::Color::Black) >= - PI / 4.)*/
-                if(0 >= tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).first()*PI
-                        && 0 < tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).last()*PI){
-                    //                it.value()->moveLeft();
-                    it.value()->setSpeedX(0);
-                }
-
-
-                //            collision avec le plafond
-                /*if(tmpTrigger.intersectAngle(IM.GetImage(hitbox), sf::Color::Black) <=  -PI/2 + PI / 4.
-                    && tmpTrigger.intersectAngle(IM.GetImage(hitbox), sf::Color::Black) >= -PI/2 - PI / 4.)*/
-                if(-PI/2 >= tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).first()*PI
-                        && -PI/2 < tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).last()*PI){
-                    it.value()->setSpeedY(0);
-                }
-
-
-            }
-            if(tmpTrigger.intersection(IM.GetImage(hitbox),sf::Color::Red)){
-                reloadLevel();
-            }
-
-            applyGravity(it,tmpTrigger);
-
-            //gestion des triggers
-            if(it.value()->get_nom() == "trigger" || it.value()->get_nom() == "triggerOn"){//operation specifique aux BinarySwitch.
-                if(it.value()->get_trigger().isIN(getHero()->get_x(),getHero()->get_y())){
-                    ((BinarySwitch*) it.value())->allowToSwitch(true);
-                }else ((BinarySwitch*) it.value())->allowToSwitch(false);
-                if(it.value()->getID() == 2 && ((BinarySwitch*) it.value())->isActivated())
-                    hitbox = "graphics/hitmap2.png";
-            }
 
 
         } //
-    }
 
-//    std::cout << getHero()->frameY << std::endl;
 
-=======
+        //    std::cout << getHero()->frameY << std::endl;
+
         //TMP TRIGGER <- TRIGGER PREVISIONNEL
         tmpTrigger = it.value()->get_trigger();
         tmpTrigger.set_centre(it.value()->get_trigger().get_centreX() + it.value()->getSpeedVector().first
                               , it.value()->get_trigger().get_centreY() + it.value()->getSpeedVector().second );
 
         //CHECK NUMERO 2 : LE CHECK PRE DEPLACEMENT
-        if(tmpTrigger.intersection(IM.GetImage("graphics/hitmap2.png"),sf::Color::Black)){
+        if(tmpTrigger.intersection(IM.GetImage(hitbox),sf::Color::Black)){
             //collision avec un mur a gauche
-            if(PI >= tmpTrigger.intersectTabAngle(IM.GetImage("graphics/hitmap2.png"), sf::Color::Black).first()*PI
-                    && PI < tmpTrigger.intersectTabAngle(IM.GetImage("graphics/hitmap2.png"), sf::Color::Black).last()*PI){
+            if(PI >= tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).first()*PI
+                    && PI < tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).last()*PI){
                 it.value()->setSpeedX(0);
             }
             //collision avec un mur a droite
-            if(0 >= tmpTrigger.intersectTabAngle(IM.GetImage("graphics/hitmap2.png"), sf::Color::Black).first()*PI
-                    && 0 < tmpTrigger.intersectTabAngle(IM.GetImage("graphics/hitmap2.png"), sf::Color::Black).last()*PI){
+            if(0 >= tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).first()*PI
+                    && 0 < tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).last()*PI){
                 it.value()->setSpeedX(0);
             }
             //            collision avec le plafond
-            if(3.* PI/2. >= tmpTrigger.intersectTabAngle(IM.GetImage("graphics/hitmap2.png"), sf::Color::Black).first()*PI
-                    && 3.* PI/2. < tmpTrigger.intersectTabAngle(IM.GetImage("graphics/hitmap2.png"), sf::Color::Black).last()*PI){
+            if(3.* PI/2. >= tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).first()*PI
+                    && 3.* PI/2. < tmpTrigger.intersectTabAngle(IM.GetImage(hitbox), sf::Color::Black).last()*PI){
                 it.value()->setSpeedY(0);
             }
         }
-        if(tmpTrigger.intersection(IM.GetImage("graphics/hitmap2.png"),sf::Color::Red)){
+        if(tmpTrigger.intersection(IM.GetImage(hitbox),sf::Color::Red)){
             reloadLevel();
         }
         if(!isDashing){
             applyGravity(it,tmpTrigger);
         }
+        //gestion des triggers
+        if(it.value()->get_nom() == "trigger" || it.value()->get_nom() == "triggerOn"){//operation specifique aux BinarySwitch.
+            if(it.value()->get_trigger().isIN(getHero()->get_x(),getHero()->get_y())){
+                ((BinarySwitch*) it.value())->allowToSwitch(true);
+            }else ((BinarySwitch*) it.value())->allowToSwitch(false);
+            if(it.value()->getID() == 2 && ((BinarySwitch*) it.value())->isActivated())
+                hitbox = "graphics/hitmap2.png";
+        }
+
     }
 
->>>>>>> bc9fb193c8626b409d8f4d86207eaa18c26efb2f
     if(abs(getHero()->getSpeedVector().first) > 0. && abs(getHero()->getSpeedVector().first) <= 2.)
         getHero()->frameY = 1;
     else if(abs(getHero()->getSpeedVector().first) > 2. && abs(getHero()->getSpeedVector().first) <=4)
@@ -241,11 +177,8 @@ void Environment::run(){
         getHero()->frameY = 3;
     else if(abs(getHero()->getSpeedVector().first) > 5)
         getHero()->frameY = 4;
-<<<<<<< HEAD
     //   else if(getHero()->getSpeedVector().first > 5)
     //       getHero()->frameY = 4;
-=======
->>>>>>> bc9fb193c8626b409d8f4d86207eaa18c26efb2f
 
     if(getHero()->getSpeedVector().first < 0 && goLeft)
         getHero()->setState("LEFT");
